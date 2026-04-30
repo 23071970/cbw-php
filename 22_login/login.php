b6c968346e5c47ad299bf99925e90f873b90d7d9
23071970 <stefan.tissot@gmail.com>
2026-04-30 12:34:33 +0200

    alan riecht

--- 22_login/login.php
+++ 22_login/login.php
@@ -1,3 +1,16 @@
+<?php
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/db/database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +23,17 @@
 <body>
     <h1>Login</h1>
     <?php include __DIR__ . '/include/partials/navi.php' ?>
+
+
+
+    <form action="" method="post">
+
+        <input type="email" name="email" value="stefan.tissot@gmail.com"><br>
+        <input type="password" name="pass"><br>
+
+        <button type="submit">Register</button>
+
+    </form>
 
 </body>
 
